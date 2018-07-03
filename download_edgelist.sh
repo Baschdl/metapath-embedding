7e29a47301a27ea26b67b5690e84901ebe8d9595
Sebastian Bischoff <sebastian.bischoff@student.hpi.de>
2018-07-04 01:14:22 +0200

    Add lower bound for node ids

--- download_edgelist.sh
+++ download_edgelist.sh
@@ -8,8 +8,9 @@ i=1
 while [ ${i} -lt  $(( (number_of_nodes+(size_of_download_in_nodes-1) ) / size_of_download_in_nodes )) ]
 do
     echo "Calculating edge list for ID(n1) up to " $((i * size_of_download_in_nodes))
-    #query=echo '{"statements":[{"statement":"MATCH (n1:Entity)--(n2:Entity) WHERE ID(n1) < ' $((i * size_of_download_in_nodes)) ' RETURN ID(n1) as n1, ID(n2) as n2"}]}'
-    query=$( echo '{"statements":[{"statement":"MATCH (n1:Entity)--(n2:Entity) WHERE ID(n1) < ' $((i * size_of_download_in_nodes)) ' RETURN ID(n1) as n1, ID(n2) as n2"}]}' )
+    query=$( echo '{"statements":[{"statement":"MATCH (n1:Entity)--(n2:Entity) WHERE ID(n1) > ' \
+        $(( (i-1) * size_of_download_in_nodes)) ' AND ID(n1) < ' $((i * size_of_download_in_nodes)) \
+        ' RETURN ID(n1) as n1, ID(n2) as n2"}]}' )
     curl -H accept:application/json -H content-type:application/json \
          -d "${query}" \
          -u neo4j:caramelBunny1998\
