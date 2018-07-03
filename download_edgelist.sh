a2180a3dfd3d21a0afbf32c6b8c74fc4da3a59bc
Sebastian Bischoff <sebastian.bischoff@student.hpi.de>
2018-07-04 00:58:47 +0200

    Correct assembly of query

--- download_edgelist.sh
+++ download_edgelist.sh
@@ -3,11 +3,15 @@ number_of_nodes=46000000
 size_of_download_in_nodes=100000
 
 i=1
+
 # Rounding up
-while i < $(( (number_of_nodes+(size_of_download_in_nodes-1) ) / size_of_download_in_nodes )); do
+while [ ${i} -lt  $(( (number_of_nodes+(size_of_download_in_nodes-1) ) / size_of_download_in_nodes )) ]
+do
     echo "Calculating edge list for ID(n1) up to " $((i * size_of_download_in_nodes))
+    #query=echo '{"statements":[{"statement":"MATCH (n1:Entity)--(n2:Entity) WHERE ID(n1) < ' $((i * size_of_download_in_nodes)) ' RETURN ID(n1) as n1, ID(n2) as n2"}]}'
+    query=$( echo '{"statements":[{"statement":"MATCH (n1:Entity)--(n2:Entity) WHERE ID(n1) < ' $((i * size_of_download_in_nodes)) ' RETURN ID(n1) as n1, ID(n2) as n2"}]}' )
     curl -H accept:application/json -H content-type:application/json \
-         -d '{"statements":[{"statement":"MATCH (p1:Entity)--(p2:Entity) WHERE ID(n1) < ' $((i * size_of_download_in_nodes)) ' RETURN ID(p1) as p1, ID(p2) as p2"}]}' \
+         -d "${query}" \
          -u neo4j:caramelBunny1998\
          http://localhost:8888/db/data/transaction/commit \
          | jq -r '(.results[0]) | .columns,.data[].row | @csv' > edgelist-wikidata-$((i * size_of_download_in_nodes)).csv
