372a5ab4176395156fe05a4b1ccf0243cc285e1d
Sebastian Bischoff <sebastian.bischoff@student.hpi.de>
2018-07-03 23:53:49 +0200

    Add loop to loop through all nodes

--- download_edgelist.sh
+++ download_edgelist.sh
@@ -1,5 +1,15 @@
-curl -H accept:application/json -H content-type:application/json \
-     -d '{"statements":[{"statement":"MATCH (p1:Entity)--(p2:Entity) WHERE ID(n1) < 100000 RETURN ID(p1) as p1, ID(p2) as p2 LIMIT 10"}]}' \
-     -u neo4j:caramelBunny1998\
-     http://localhost:8888/db/data/transaction/commit \
-     | jq -r '(.results[0]) | .columns,.data[].row | @csv' > edgelist-wikidata-100000.csv
+#!/usr/bin/env bash
+number_of_nodes=46000000
+size_of_download_in_nodes=100000
+
+i=1
+# Rounding up
+while i < $(( (number_of_nodes+(size_of_download_in_nodes-1) ) / size_of_download_in_nodes )); do
+    echo "Calculating edge list for ID(n1) up to " $((i * size_of_download_in_nodes))
+    curl -H accept:application/json -H content-type:application/json \
+         -d '{"statements":[{"statement":"MATCH (p1:Entity)--(p2:Entity) WHERE ID(n1) < ' $((i * size_of_download_in_nodes)) ' RETURN ID(p1) as p1, ID(p2) as p2"}]}' \
+         -u neo4j:caramelBunny1998\
+         http://localhost:8888/db/data/transaction/commit \
+         | jq -r '(.results[0]) | .columns,.data[].row | @csv' > edgelist-wikidata-$((i * size_of_download_in_nodes)).csv
+    i=$((i + 1))
+done
