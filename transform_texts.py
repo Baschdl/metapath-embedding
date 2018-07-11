a2e5540c3640cd040e0c406417f69f56639b5824
Sebastian Bischoff <sebastian.bischoff@student.hpi.de>
2018-07-11 16:54:45 +0200

    Parallelize conversion

--- transform_texts.py
+++ transform_texts.py
@@ -3,6 +3,7 @@ from two_class_dict import TwoWayDict
 import time
 import argparse
 from tqdm import tqdm
+from multiprocessing import Pool
 
 
 class Converter():
@@ -33,42 +34,26 @@ class Converter():
                     i += 1
         return max_node_id
 
-    def convert_file(self, infile_path: str, outfile_path: str, max_node_id: int):
+    @staticmethod
+    def convert_file(infile_path: str, outfile_path: str, max_node_id: int):
         with open(infile_path, "r") as infile:
             with open(outfile_path, "w") as outfile:
                 for line in infile:
                     line_separated = Converter.split_line(line)
                     line_utf = []
                     i = 0
-                    for char_number in line_separated:
+                    for number_as_char in line_separated:
                         try:
-                            number = int(char_number)
+                            number = int(number_as_char)
                         except ValueError as e:
                             print("Error for file {} in line '{}'".format(infile_path, line))
                             print(e)
                         if i % 2 == 0:
-                            self.d[number] = chr(number)
-                            line_utf.append(self.d[number])
+                            line_utf.append(chr(number))
                         else:
-                            self.d[number + max_node_id] = chr(number + max_node_id)
-                            line_utf.append(self.d[number + max_node_id])
+                            line_utf.append(chr(number + max_node_id))
                         i += 1
                     outfile.write("".join(line_utf) + "\n")
-
-    def check_bijection(self, file_original: str, file_converted: str, max_node_id: int):
-        with open(file_converted, "r") as infile_converted:
-            with open(file_original, "r") as infile_original:
-                for line_converted, line_original in zip(infile_converted, infile_original):
-                    i = 0
-                    for char_converted, char_original in zip(line_converted, line_original[:-1].split("|")):
-                        if i % 2 == 0:
-                            assert self.d[char_converted] == int(char_original), str(
-                                self.d[char_converted]) + "!=" + str(
-                                char_original) + " for nodes"
-                        else:
-                            assert self.d[char_converted] - max_node_id == int(char_original), str(
-                                self.d[char_converted + max_node_id]) + "!=" + str(char_original) + " for edges"
-                        i += 1
 
 
 def parse_arguments():
@@ -77,10 +62,10 @@ def parse_arguments():
                         help='Path where meta-path files are located',
                         type=str,
                         required=True)
-    parser.add_argument('--check_bijection',
-                        help="Enable checking that the utf8 file can be converted back to the original file",
-                        default=False,
-                        type=bool)
+    parser.add_argument('--processes',
+                        help='Specify number of processes which should be used for the conversion',
+                        type=int,
+                        required=True)
 
     return parser.parse_args()
 
@@ -89,35 +74,32 @@ if __name__ == "__main__":
     args = parse_arguments()
 
     converter = Converter()
-    max_node_id = -1
     start = time.time()
-    for file in tqdm(os.listdir(args.dirpath)):
-        if not '_converted.txt' in file:
-            file_max_node_id = converter.find_max_node_id(os.path.join(args.dirpath, file))
-            if file_max_node_id > max_node_id:
-                max_node_id = file_max_node_id
+
+    pool = Pool(processes=args.processes)
+    files = [os.path.join(args.dirpath, file) for file in os.listdir(args.dirpath) if not '_converted.txt' in file]
+    results = []
+    for x in tqdm.tqdm(pool.map(Converter.find_max_node_id, files), total=len(files)):
+        results.append(x)
+    max_node_id = -1
+    for file_max_node_id in results:
+        if file_max_node_id > max_node_id:
+            max_node_id = file_max_node_id
     end = time.time()
     print("Max node time: {}".format(end - start))
     print('Max_node_id is {}'.format(max_node_id))
 
     start = time.time()
-    for file in tqdm(os.listdir(args.dirpath)):
-        if not '_converted.txt' in file or os.path.exists(file[:-4] + '_converted.txt'):
-            converter.convert_file(os.path.join(args.dirpath, file),
-                                   os.path.join(args.dirpath, file[:-4] + '_converted.txt'),
-                                   max_node_id)
+    args = [(os.path.join(args.dirpath, file),
+             os.path.join(args.dirpath, file[:-4] + '_converted.txt'),
+             max_node_id)
+            for file in os.listdir(args.dirpath) if
+            not '_converted.txt' in file or os.path.exists(file[:-4] + '_converted.txt')]
+
+    for _ in tqdm.tqdm(pool.map(Converter.convert_file, args), total=len(args)):
+        pass
+
     end = time.time()
     print("Converting time is {}".format(end - start))
-
-    if args.check_bijection:
-        print("Reversing...")
-        start = time.time()
-        for file in tqdm(os.listdir(args.dirpath)):
-            if not '_converted.txt' in file:
-                converter.check_bijection(os.path.join(args.dirpath, file),
-                                          os.path.join(args.dirpath, file[:-4] + "_converted.txt"),
-                                          max_node_id)
-        end = time.time()
-        print("Check bijection time is {}".format(end - start))
 
     print("Finished")
