dd4e5d0cd022cc5a2580667e0da432f43105c796
Sebastian Bischoff <sebastian.bischoff@student.hpi.de>
2018-07-13 18:22:51 +0200

    Add script to find negative edges

---
+++ find_neg_edges.py
@@ -0,0 +1,71 @@
+import argparse
+from tqdm import tqdm
+from find_new_edges import NewEdgeFinder
+from neo4j.v1 import GraphDatabase
+import os
+from find_new_edges import NewEdgeFinder
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('--folder_mined_metapaths',
+                        help='Folder where the files with the mined meta-paths lie',
+                        type=str,
+                        required=True)
+    parser.add_argument('--converted_new_edges_list',
+                        help='Path to save converted "new edges" list',
+                        type=str,
+                        required=True)
+    parser.add_argument('--neg_edges_list',
+                        help='Path where to save the negative edges list',
+                        type=str,
+                        required=True)
+    parser.add_argument('--uri',
+                        help='Bolt address of neo4j instance',
+                        type=str,
+                        required=True)
+    parser.add_argument('--user',
+                        help='User name of neo4j instance',
+                        type=str,
+                        required=True)
+    parser.add_argument('--password',
+                        help='Password of neo4j instance',
+                        type=str,
+                        required=True)
+
+    return parser.parse_args()
+
+
+def parse_ids_from_filename(filename: str):
+    _, id1, id2 = (filename.split("-")[-1]).split("_")
+    id2 = id2[:-4]
+    return int(id1), int(id2)
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    neo = GraphDatabase.driver(args.uri, auth=(args.user, args.password))
+    neg_edges = []
+    new_edges = {}
+
+    print("Loading new edges...")
+    with open(args.converted_new_edges_list, 'r') as file:
+        for line in tqdm(file):
+            node1, node2 = NewEdgeFinder.split_edge(line)
+            new_edges[(node1, node2)] = True
+
+    print("Finding neg edges...")
+    for file in tqdm(os.listdir(args.folder_mined_metapaths)):
+        if "_converted.txt" in file:
+            pass
+        id1, id2 = parse_ids_from_filename(file)
+        with neo.session() as session:
+            result = session.run('MATCH (n1)--(n2) WHERE ID(n1) = {} AND ID(n2) = {} RETURN n1, n2'.format(id1, id2))
+            result = [rec["n1, n2"] for rec in result]
+            if not result and not new_edges.get((id1, id2), False):
+                neg_edges.append((id1, id2))
+
+    print("Writing to disk...")
+    with open(args.neg_edges_list, 'w') as file:
+        for node1, node2 in neg_edges:
+            file.write("{} {}\n".format(node1, node2))
---
+++ tests/test_parse_ids_from_filename.py
@@ -0,0 +1,7 @@
+from unittest import TestCase
+from find_neg_edges import parse_ids_from_filename
+
+
+class TestParse_ids_from_filename(TestCase):
+    def test_parse_ids_from_filename(self):
+        self.assertEqual(parse_ids_from_filename("MetaPaths-5-0.8_0_17414227.txt"), (0, 17414227))
