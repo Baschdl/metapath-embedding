3c6b57ca202deef34852fc97799a6244a264734b
Sebastian Bischoff <sebastian.bischoff@student.hpi.de>
2018-07-27 02:23:56 +0200

    Add scripts for nodes and relations

--- download-nodes.sh
+++ download-nodes.sh
@@ -8,13 +8,13 @@ i=1
 while [ ${i} -lt  $(( (number_of_nodes+(size_of_download_in_nodes-1) ) / size_of_download_in_nodes )) ]
 do
     echo "Calculating edge list for ID(n1) up to " $((i * size_of_download_in_nodes))
-    query=$( echo '{"statements":[{"statement":"MATCH (n1:Entity)-[r]-(n2:Entity) WHERE ID(n1) > ' \
+    query=$( echo '{"statements":[{"statement":"MATCH (n1:Entity) WHERE ID(n1) > ' \
         $(( (i-1) * size_of_download_in_nodes)) ' AND ID(n1) < ' $((i * size_of_download_in_nodes)) \
-        ' RETURN ID(n1) as n1, ID(r), ID(n2) as n2"}]}' )
+        ' RETURN ID(n1) as n1"}]}' )
     curl -H accept:application/json -H content-type:application/json \
          -d "${query}" \
          -u neo4j:caramelBunny1998\
          http://"$1":"$2"/db/data/transaction/commit \
-         | jq -r '(.results[0]) | .columns,.data[].row | @csv' > edgelist-wikidata-triplets-$((i * size_of_download_in_nodes)).csv
+         | jq -r '(.results[0]) | .columns,.data[].row | @csv' > edgelist-wikidata-nodes-$((i * size_of_download_in_nodes)).csv
     i=$((i + 1))
 done
---
+++ download-relations.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+number_of_edges=409000000
+size_of_download_in_edges=100000
+
+i=1
+
+# Rounding up
+while [ ${i} -lt  $(( (number_of_edges+(size_of_download_in_edges-1) ) / size_of_download_in_edges )) ]
+do
+    echo "Calculating relation list for ID(r) up to " $((i * size_of_download_in_edges))
+    query=$( echo '{"statements":[{"statement":"MATCH ()-[r]-() WHERE ID(r) > ' \
+        $(( (i-1) * size_of_download_in_edges)) ' AND ID(r) < ' $((i * size_of_download_in_edges)) \
+        ' RETURN ID(r) as r"}]}' )
+    curl -H accept:application/json -H content-type:application/json \
+         -d "${query}" \
+         -u neo4j:caramelBunny1998\
+         http://"$1":"$2"/db/data/transaction/commit \
+         | jq -r '(.results[0]) | .columns,.data[].row | @csv' > edgelist-wikidata-relations-$((i * size_of_download_in_edges)).csv
+    i=$((i + 1))
+done
