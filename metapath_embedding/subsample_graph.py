f1e2e27d607414cd726d9f5454e4497f7c0831ae
Sebastian Bischoff <sebastian.bischoff@student.hpi.de>
2018-07-28 15:47:34 +0200

    Correct mode for filtered file

--- metapath_embedding/subsample_graph.py
+++ metapath_embedding/subsample_graph.py
@@ -17,7 +17,7 @@ def filter(all_edges_filepath, filtered_edges_filepath, part_of_nodes):
 
     nodes_subsample = random.sample(nodes, int(part_of_nodes * len(nodes)))
     filtered_edges = []
-    with open(filtered_edges_filepath, 'r') as all_edges_file:
+    with open(filtered_edges_filepath, 'w') as all_edges_file:
         for edge in edges:
             if edge[0] in nodes_subsample and edge[1] in nodes_subsample:
                 all_edges_file.write("{} {}\n".format(edge[0], edge[1]))
