2c9aa5e2bfb37628209bcea0347b3beff41a7066
Sebastian Bischoff <sebastian.bischoff@student.hpi.de>
2018-07-28 21:10:38 +0200

    Write multiple subsamples

--- metapath_embedding/subsample_graph.py
+++ metapath_embedding/subsample_graph.py
@@ -1,11 +1,13 @@
 import random
 import argparse
+from multiprocessing.pool import Pool
+
 from tqdm import tqdm
 
 from metapath_embedding.find_new_edges import NewEdgeFinder
 
 
-def filter(all_edges_filepath, filtered_edges_filepath, part_of_nodes):
+def filter(all_edges_filepath, filtered_edges_filepath, part_of_nodes, number_of_subsamples):
     print("Read all nodes and edges...")
     edges = []
     nodes = set()
@@ -16,10 +18,20 @@ def filter(all_edges_filepath, filtered_edges_filepath, part_of_nodes):
             edges.append((node0, node1))
             nodes.add(node0)
             nodes.add(node1)
-    
+
     print("Sample nodes...")
-    nodes_subsample = random.sample(nodes, int(part_of_nodes * len(nodes)))
+    nodes_subsamples = []
+    for i in range(number_of_subsamples):
+        nodes_subsamples.append(random.sample(nodes, int(part_of_nodes * len(nodes))))
     print("Write new edges to file...")
+    args = []
+    for subsample in nodes_subsamples:
+        args.append((edges, filtered_edges_filepath, subsample))
+    pool = Pool()
+    pool.map(write_edgelist_to_file, args)
+
+def write_edgelist_to_file(args):
+    edges, filtered_edges_filepath, nodes_subsample = args
     with open(filtered_edges_filepath, 'w') as all_edges_file:
         for edge in tqdm(edges):
             if edge[0] in nodes_subsample and edge[1] in nodes_subsample:
@@ -37,10 +49,13 @@ def parse_arguments():
     parser.add_argument('--part_of_nodes',
                         type=float,
                         required=True)
+    parser.add_argument('--number_of_subsamples',
+                        type=int,
+                        required=True)
 
     return parser.parse_args()
 
 
 if __name__ == "__main__":
     args = parse_arguments()
-    filter(args.all_edges_filepath, args.filtered_edges_filepath, args.part_of_nodes)
+    filter(args.all_edges_filepath, args.filtered_edges_filepath, args.part_of_nodes, number_of_subsamples)
